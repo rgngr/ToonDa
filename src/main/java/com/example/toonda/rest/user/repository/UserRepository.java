2fe56b1c60868909233def0d3c8381a7baee9e74
rgngr <wlals8369@naver.com>
2023-03-18 02:10:43 +0900

    Docs : 주석 추가 (#4)

--- src/main/java/com/example/toonda/rest/user/repository/UserRepository.java
+++ src/main/java/com/example/toonda/rest/user/repository/UserRepository.java
@@ -9,7 +9,7 @@ import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<User> findByKakaoIdIsNullAndEmail(String email);
+    Optional<User> findByKakaoIdNullAndEmail(String email);
 
     boolean existsByEmail(String email);
 
