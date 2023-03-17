d80e832f042a6009042a419e36064c4697c31d2f
rgngr <wlals8369@naver.com>
2023-03-18 01:18:58 +0900

    Chore : login response swagger (#4)

--- src/main/java/com/example/toonda/rest/user/dto/LoginResponseDto.java
+++ src/main/java/com/example/toonda/rest/user/dto/LoginResponseDto.java
@@ -1,15 +1,20 @@
 package com.example.toonda.rest.user.dto;
 
 import com.example.toonda.rest.user.entity.User;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 @Getter
 @RequiredArgsConstructor
+@Schema(description = "로그인 response")
 public class LoginResponseDto {
 
+    @Schema (description = "userId")
     private Long userId;
+    @Schema (description = "유저네임")
     private String username;
+    @Schema (description = "프로필 사진")
     private String userImg;
 
     public LoginResponseDto(User user) {
