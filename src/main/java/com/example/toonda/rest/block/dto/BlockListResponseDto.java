21894fea82863ebfcccf78078198bd98eef17b16
rgngr <wlals8369@naver.com>
2023-03-18 20:47:57 +0900

    Refactor : BlockResponseDto 를 BlockListResponseDto 내 중첩 클래스로 위치 변경 (#29)

--- src/main/java/com/example/toonda/rest/block/dto/BlockListResponseDto.java
+++ src/main/java/com/example/toonda/rest/block/dto/BlockListResponseDto.java
@@ -1,10 +1,15 @@
 package com.example.toonda.rest.block.dto;
 
+import com.example.toonda.rest.user.entity.User;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import java.util.ArrayList;
 import java.util.List;
 
 @Getter
+@NoArgsConstructor
+@Schema(description = "차단 리스트 response")
 public class BlockListResponseDto {
 
     private List<BlockResponseDto> blockList = new ArrayList<>();
@@ -13,8 +18,20 @@ public class BlockListResponseDto {
         blockList.add(responseDto);
     }
 
-    public BlockListResponseDto() {
+    @Getter
+    @Schema(description = "차단 낱개 response")
+    public static class BlockResponseDto {
 
+        private Long userId;
+        private String userImg;
+        private String username;
+        // 차단 여부도 필요하려나.......?
+
+        public BlockResponseDto(User user) {
+            this.userId = user.getId();
+            this.userImg = user.getImg();
+            this.username = user.getUsername();
+        }
     }
 
 }
