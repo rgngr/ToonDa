b00c5e92c8db2bb1f8094bb59863b53c2c212582
rgngr <wlals8369@naver.com>
2023-03-18 20:48:52 +0900

    Feat : 차단 리스트 조건에 탈퇴한 회원 제외 추가 (#29)

--- src/main/java/com/example/toonda/rest/block/service/BlockService.java
+++ src/main/java/com/example/toonda/rest/block/service/BlockService.java
@@ -5,7 +5,6 @@ import com.example.toonda.config.exception.RestApiException;
 import com.example.toonda.config.exception.errorcode.Code;
 import com.example.toonda.config.security.SecurityUtil;
 import com.example.toonda.rest.block.dto.BlockListResponseDto;
-import com.example.toonda.rest.block.dto.BlockResponseDto;
 import com.example.toonda.rest.block.entity.Block;
 import com.example.toonda.rest.block.repository.BlockRepository;
 import com.example.toonda.rest.user.entity.User;
@@ -31,28 +30,27 @@ public class BlockService {
         User blockedUser = userRepository.findByIdAndDeletedFalse(userId).orElseThrow(() -> new RestApiException(Code.NO_USER));
         // 차단 여부 확인 >> 차단/취소
         Block blockExistence = blockRepository.findByUserAndBlockedUser(user, blockedUser).orElse(null);
-        boolean isBlocked;
+        boolean blockOrNot;
         if (blockExistence == null) {
             blockExistence = blockRepository.save(new Block(user, blockedUser));
-            isBlocked = true;
+            blockOrNot= true;
         } else {
             blockRepository.delete(blockExistence);
-            isBlocked = false;
+            blockOrNot= false;
         }
-        return ResponseDto.of(isBlocked, Code.BLOCK_OR_CANCEL);
+        return ResponseDto.of(blockOrNot, Code.BLOCK_OR_CANCEL);
     }
 
-    // 탈퇴한 회원 처리........어떻게.......?
-    @Transactional
+    @Transactional(readOnly = true)
     public BlockListResponseDto blockList() {
         // 유저 확인
         User user = SecurityUtil.getCurrentUser();
         if (user == null) throw new RestApiException(Code.NOT_FOUND_AUTHORIZATION_IN_SECURITY_CONTEXT);
-        // 차단 리스트 생성
+        // 차단 리스트 생성 (deleted = false, order by blockId)
         BlockListResponseDto blockListResponseDto = new BlockListResponseDto();
         List<User> blockedUsers = userRepository.findAllBlockedUsers(user);
         for (User blockedUser : blockedUsers) {
-            blockListResponseDto.addBlockedUser(new BlockResponseDto(blockedUser));
+            blockListResponseDto.addBlockedUser(new BlockListResponseDto.BlockResponseDto(blockedUser));
         }
         return blockListResponseDto;
     }
--- src/main/java/com/example/toonda/rest/user/repository/UserRepository.java
+++ src/main/java/com/example/toonda/rest/user/repository/UserRepository.java
@@ -18,6 +18,6 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByIdAndDeletedFalse(Long userId);
 
-    @Query("select u from Users u join Block b on b.blockedUser = u where b.user = :user")
+    @Query("select u from Users u join Block b on b.blockedUser = u where b.user = :user and u.deleted = false order by b.id")
     List<User> findAllBlockedUsers(@Param("user") User user);
 }
