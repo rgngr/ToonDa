77ad85e4ce32a9c9a91b81db5ef51c307c0584ff
rgngr <wlals8369@naver.com>
2023-03-21 00:19:50 +0900

    Feat : 폴더 선택 리스트 api 완성 외 (#15) - Service logic 일분 변경 - Request, Response 정리 - id >> diaryId 변경

--- src/main/java/com/example/toonda/rest/diary/controller/DiaryController.java
+++ src/main/java/com/example/toonda/rest/diary/controller/DiaryController.java
@@ -15,41 +15,47 @@ import java.io.IOException;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/folders")
-@Tag(name = "diaries", description = "다이어리 관련 API")
+@Tag(name = "diaries", description = "다이어리 생성/ get 수정 페이지/ 수정/ 삭제, 폴더 선택, 다이어리 리스트")
 public class DiaryController {
 
     private final DiaryService diaryService;
 
-    @ApiOperation(value = "다이어리 생성")
+    @ApiOperation("다이어리 생성")
     @PostMapping( "/{folderId}/diaries")
     public ResponseDto createDiary(@PathVariable Long folderId, @ModelAttribute @Valid DiaryRequestDto requestDto) throws IOException {
         return DataResponseDto.of(diaryService.createDiary(folderId, requestDto), Code.CREATE_DIARY.getStatusMsg());
     }
 
-//    @ApiOperation(value = "폴더 선택")
-//    @GetMapping("/diaries/select-folder")
-//    public ResponseDto getFolders() {
-//        return DataResponseDto.of(diaryService.getFolders(), Code.GET_FOLDER_LIST.getStatusMsg());
-//    }
-
-    @ApiOperation(value = "GET 다이어리 수정 페이지")
-    @GetMapping("/diaries/{id}")
-    public ResponseDto getDiaryUpdatePage(@PathVariable Long id) {
-        return DataResponseDto.of(diaryService.getDiaryUpdatePage(id), Code.GET_DIARY_UPDATE_PAGE.getStatusMsg());
+    @ApiOperation("폴더 선택 리스트")
+    @GetMapping("/diaries/folder-list")
+    public ResponseDto getFolderList() {
+        return DataResponseDto.of(diaryService.getFolderList(), Code.GET_FOLDERS.getStatusMsg());
     }
 
-    @ApiOperation(value = "다이어리 수정")
-    @PatchMapping("/diaries/{id}")
-    public ResponseDto updateDiary(@PathVariable Long id, @RequestBody @Valid DiaryRequestDto.Update requestDto) {
-        diaryService.updateDiary(id, requestDto);
+    @ApiOperation("GET 다이어리 수정 페이지")
+    @GetMapping("/diaries/{diaryId}")
+    public ResponseDto getDiaryUpdatePage(@PathVariable Long diaryId) {
+        return DataResponseDto.of(diaryService.getDiaryUpdatePage(diaryId), Code.GET_DIARY_UPDATE_PAGE.getStatusMsg());
+    }
+
+    @ApiOperation("다이어리 수정")
+    @PatchMapping("/diaries/{diaryId}")
+    public ResponseDto updateDiary(@PathVariable Long diaryId, @RequestBody @Valid DiaryRequestDto.Update requestDto) {
+        diaryService.updateDiary(diaryId, requestDto);
         return ResponseDto.of(true, Code.UPDATE_DIARY);
     }
 
-    @ApiOperation(value = "다이어리 삭제")
-    @DeleteMapping("/diaries/{id}")
-    public ResponseDto deleteDiary(@PathVariable Long id) {
-        diaryService.deleteDiary(id);
+    @ApiOperation("다이어리 삭제")
+    @DeleteMapping("/diaries/{diaryId}")
+    public ResponseDto deleteDiary(@PathVariable Long diaryId) {
+        diaryService.deleteDiary(diaryId);
         return ResponseDto.of(true, Code.DELETE_DIARY);
     }
+
+//    @ApiOperation("다이어리 리스트")
+//    @GetMapping("/{folderId}/diary-list")
+//    public ResponseDto getDiaryList(@PathVariable Long folderId) {
+//        return DataResponseDto.of(diaryService.getDiaryList(folderId), Code.GET_DIARIES.getStatusMsg());
+//    }
 
 }
--- src/main/java/com/example/toonda/rest/diary/service/DiaryService.java
+++ src/main/java/com/example/toonda/rest/diary/service/DiaryService.java
@@ -8,13 +8,13 @@ import com.example.toonda.rest.diary.dto.DiaryRequestDto;
 import com.example.toonda.rest.diary.dto.DiaryResponseDto;
 import com.example.toonda.rest.diary.entity.Diary;
 import com.example.toonda.rest.diary.repository.DiaryRepository;
+import com.example.toonda.rest.folder.dto.FolderResponseDto;
 import com.example.toonda.rest.folder.entity.Folder;
 import com.example.toonda.rest.folder.repository.FolderRepository;
 import com.example.toonda.rest.user.entity.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
 import java.util.List;
 
@@ -26,75 +26,83 @@ public class DiaryService {
     private final DiaryRepository diaryRepository;
     private final S3Uploader s3Uploader;
 
+    // 다이어리 생성
     @Transactional
     public DiaryResponseDto createDiary(Long folderId, DiaryRequestDto requestDto) throws IOException {
-        // 유저 확인
+        // 로그인 여부 확인
         User user = SecurityUtil.getCurrentUser();
         if (user == null) throw new RestApiException(Code.NOT_FOUND_AUTHORIZATION_IN_SECURITY_CONTEXT);
-
-        Folder folder = folderRepository.findById(folderId).orElseThrow(() -> new RestApiException(Code.NO_FOLDER));
-
+        // 폴더 존재 여부 확인 (deleted = false)
+        Folder folder = folderRepository.findByIdAndDeletedFalse(folderId).orElseThrow(() -> new RestApiException(Code.NO_FOLDER));
+        // 폴더 작성자 본인 여부 확인
+        if (user.getId() != folder.getUser().getId()) throw new RestApiException(Code.INVALID_USER);
+        // 다이어리 생성
         String diaryImg = s3Uploader.upload(requestDto.getImg(), "file");
         Diary diary = diaryRepository.save(new Diary(user, folder, requestDto, diaryImg));
 
         return new DiaryResponseDto(user, diary);
     }
 
+    // 폴더 선택 리스트
     @Transactional(readOnly = true)
-    public DiaryResponseDto getDiaryUpdatePage(Long id) {
-        // 유저 확인
+    public DiaryResponseDto.FolderList getFolderList() {
+        // 로그인 여부 확인
         User user = SecurityUtil.getCurrentUser();
         if (user == null) throw new RestApiException(Code.NOT_FOUND_AUTHORIZATION_IN_SECURITY_CONTEXT);
-
-        Diary diary = diaryRepository.findById(id).orElseThrow(() -> new RestApiException(Code.NO_DIARY));
-
-        if (user.getId() != diary.getUser().getId()) {
-            throw new RestApiException(Code.INVALID_USER);
-        } else {
-            return new DiaryResponseDto(user, diary);
+        // 폴더 리스트 생성 (delete = false)
+        DiaryResponseDto.FolderList folderList = new DiaryResponseDto.FolderList();
+        List<Folder> folders = folderRepository.findAllUsersFolders(user);
+        for (Folder folder : folders) {
+            FolderResponseDto.Title title = new FolderResponseDto.Title(folder);
+            folderList.addFolder(title);
         }
+        return folderList;
     }
 
+    // get 다이어리 수정 페이지
+    @Transactional(readOnly = true)
+    public DiaryResponseDto getDiaryUpdatePage(Long diaryId) {
+        // 로그인 여부 확인
+        User user = SecurityUtil.getCurrentUser();
+        if (user == null) throw new RestApiException(Code.NOT_FOUND_AUTHORIZATION_IN_SECURITY_CONTEXT);
+        // 다이어리 존재 여부 확인 (deleted = false)
+        Diary diary = diaryRepository.findById(diaryId).orElseThrow(() -> new RestApiException(Code.NO_DIARY));
+        // 다이어리 작성자 여부 확인
+        if (user.getId() != diary.getUser().getId()) throw new RestApiException(Code.INVALID_USER);
+        // 다이어리 수정 페이지 생성
+        return new DiaryResponseDto(user, diary);
+    }
+
+    // 다이어리 수정
     @Transactional
-    public void updateDiary(Long id, DiaryRequestDto.Update requestDto) {
-        // 유저 확인
+    public void updateDiary(Long diaryId, DiaryRequestDto.Update requestDto) {
+        // 로그인 여부 확인
         User user = SecurityUtil.getCurrentUser();
         if (user == null) throw new RestApiException(Code.NOT_FOUND_AUTHORIZATION_IN_SECURITY_CONTEXT);
-
-        Diary diary = diaryRepository.findById(id).orElseThrow(() -> new RestApiException(Code.NO_DIARY));
-
-        if (user.getId() != diary.getUser().getId()) {
-            throw new RestApiException(Code.INVALID_USER);
-        } else {
-            diary.updateDiary(requestDto);
-        }
-
+        // 다이어리 존재 여부 확인 (deleted = false)
+        Diary diary = diaryRepository.findById(diaryId).orElseThrow(() -> new RestApiException(Code.NO_DIARY));
+        // 다이어리 작성자 여부 확인
+        if (user.getId() != diary.getUser().getId()) throw new RestApiException(Code.INVALID_USER);
+        // 다이어리 수정
+        diary.updateDiary(requestDto);
     }
 
+    // 다이어리 삭제
     @Transactional
-    public void deleteDiary(Long id) {
-        // 유저 확인
+    public void deleteDiary(Long diaryId) {
+        // 로그인 여부 확인
         User user = SecurityUtil.getCurrentUser();
         if (user == null) throw new RestApiException(Code.NOT_FOUND_AUTHORIZATION_IN_SECURITY_CONTEXT);
-
-        Diary diary = diaryRepository.findById(id).orElseThrow(() -> new RestApiException(Code.NO_DIARY));
-
-        if (user.getId() != diary.getUser().getId()) {
-            throw new RestApiException(Code.INVALID_USER);
-        } else {
-            String diaryImg = diary.getImg();
-            s3Uploader.deleteFile(diaryImg.split(".com/")[1]);
-            diary.deleteDiary();
-
-        }
+        // 다이어리 존재 여부 확인 (deleted = false)
+        Diary diary = diaryRepository.findById(diaryId).orElseThrow(() -> new RestApiException(Code.NO_DIARY));
+        // 다이어리 작성자 여부 확인
+        if (user.getId() != diary.getUser().getId()) throw new RestApiException(Code.INVALID_USER);
+        // 다이어리 삭제
+        String diaryImg = diary.getImg();
+        s3Uploader.deleteFile(diaryImg.split(".com/")[1]);
+        diary.deleteDiary();
     }
 
-//    @Transactional(readOnly = true)
-//    public DiaryResponseDto.Folders getFolders() {
-//        // 유저 확인
-//        User user = SecurityUtil.getCurrentUser();
-//        if (user == null) throw new RestApiException(Code.NOT_FOUND_AUTHORIZATION_IN_SECURITY_CONTEXT);
-//
-//        List<Folder> folders = folderRepository.findByUser(user)
-//    }
+    // 다이어리 리스트
+
 }
